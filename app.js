ad8a8eb827430b710228cba06cdd9abadeff6843
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-06-22 02:54:13 -0400

    changes with diployment

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ app.use('/api/availability', availabilityRoutes);
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 
 app.use(notFoundMiddleware);
